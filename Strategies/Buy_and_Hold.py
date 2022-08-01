c7fd729d8bb2d1d515abcae4d74271cbd2a9624a
Lager <dm.30314@gmail.com>
2022-08-01 17:34:20 +0800

    b&h, trading record

--- Strategies/Buy_and_Hold.py
+++ Strategies/Buy_and_Hold.py
@@ -4,9 +4,9 @@ import datetime
 
 class Buy_and_Hold(Strategy):
     def back_test(self, data):
-        self.open_price = data["open"][0]
-        self.close_price = data["close"][len(data) - 1]
-        diff = self.open_price - self.close_price
-        return diff / self.open_price
+        open_price = data["open"][0]
+        close_price = data["close"][len(data) - 1]
+        diff = open_price - close_price
+        return diff / open_price, 2, [[0], [open_price]], [[len(data) - 1], [close_price]]
     
     
